460ce9faa875742206a68511f50c963e5302dbea
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-12-26 13:22:45 +0100

    add Function1Test

---
+++ src/test/java/org/kleinb/jfun/Function1Test.java
@@ -0,0 +1,46 @@
+package org.kleinb.jfun;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class Function1Test {
+
+  @Test
+  void constant() {
+    Function1<String, String> f = Function1.constant("foo");
+    assertThat(f.apply("bar")).isEqualTo("foo");
+  }
+
+  @Test
+  void identity() {
+    Function1<String, String> f = Function1.identity();
+    assertThat(f.apply("foo")).isEqualTo("foo");
+  }
+
+  @Test
+  void curried() {
+    Function1<String, String> f = a -> "foo";
+    assertThat(f.curried().apply("bar")).isEqualTo("foo");
+  }
+
+  @Test
+  void tupled() {
+    Function1<String, String> f = a -> "foo";
+    assertThat(f.tupled().apply(Tuple.of("bar"))).isEqualTo("foo");
+  }
+
+  @Test
+  void compose() {
+    Function1<String, String> f = a -> "f" + a;
+    Function1<String, String> g = a -> "g" + a;
+    assertThat(f.compose(g).apply("h")).isEqualTo("fgh");
+  }
+
+  @Test
+  void andThen() {
+    Function1<String, String> f = a -> "f" + a;
+    Function1<String, String> g = a -> "g" + a;
+    assertThat(f.andThen(g).apply("h")).isEqualTo("ghf");
+  }
+}
