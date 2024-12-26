49fb9cafc457cc12a58c8354d5bf5609c51061f3
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-12-26 19:38:10 +0100

    fmt

--- src/main/java/org/kleinb/jfun/Tuple.java
+++ src/main/java/org/kleinb/jfun/Tuple.java
@@ -2,6 +2,7 @@ package org.kleinb.jfun;
 
 sealed interface Tuple
     permits Tuple0, Tuple1, Tuple2, Tuple3, Tuple4, Tuple5, Tuple6, Tuple7, Tuple8 {
+
   static Tuple0 empty() {
     return Tuple0.INSTANCE;
   }
