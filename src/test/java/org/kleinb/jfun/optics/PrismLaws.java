5a152cfc3e37a703927462e90b54bd746e1ead52
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-12-28 10:58:32 +0100

    extract optics laws

--- src/test/java/org/kleinb/jfun/optics/PrismLaws.java
+++ src/test/java/org/kleinb/jfun/optics/PrismLaws.java
@@ -5,18 +5,18 @@ import org.kleinb.jfun.Option;
 import org.kleinb.jfun.Some;
 
 public interface PrismLaws {
-    static <S, A> boolean partialRoundTripOneWay(Prism<S, A> p, S s) {
-        switch (p.getOption(s)) {
-            case Some(A value) -> {
-                return p.reverseGet(value).equals(s);
-            }
-            case None<A> _ -> {
-                return true;
-            }
-        }
+  static <S, A> boolean partialRoundTripOneWay(Prism<S, A> p, S s) {
+    switch (p.getOption(s)) {
+      case Some(A value) -> {
+        return p.reverseGet(value).equals(s);
+      }
+      case None<A> _ -> {
+        return true;
+      }
     }
+  }
 
-    static <A, B> boolean partialRoundTripOtherWay(Prism<A, B> p, B b) {
-        return p.getOption(p.reverseGet(b)).equals(Option.some(b));
-    }
+  static <A, B> boolean partialRoundTripOtherWay(Prism<A, B> p, B b) {
+    return p.getOption(p.reverseGet(b)).equals(Option.some(b));
+  }
 }
