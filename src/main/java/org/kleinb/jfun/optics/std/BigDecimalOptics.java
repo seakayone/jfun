7bca9be0455b2f7fe21f651c08020ac80056118e
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-12-29 13:18:32 +0100

    add BigDecimalOptics

---
+++ src/main/java/org/kleinb/jfun/optics/std/BigDecimalOptics.java
@@ -0,0 +1,17 @@
+package org.kleinb.jfun.optics.std;
+
+import java.math.BigDecimal;
+import org.kleinb.jfun.Try;
+import org.kleinb.jfun.optics.Prism;
+
+public class BigDecimalOptics {
+  private BigDecimalOptics() {}
+
+  public static Prism<BigDecimal, Long> bigDecimalToLong() {
+    return Prism.of(bd -> Try.of(bd::longValue).toOption(), BigDecimal::new);
+  }
+
+  public static Prism<BigDecimal, Integer> bigDecimalToInteger() {
+    return Prism.of(bd -> Try.of(bd::intValue).toOption(), BigDecimal::new);
+  }
+}
---
+++ src/test/java/org/kleinb/jfun/optics/std/BigDecimalOpticsTest.java
@@ -0,0 +1,41 @@
+package org.kleinb.jfun.optics.std;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import org.junit.jupiter.api.Test;
+import org.kleinb.jfun.optics.Prism;
+import org.kleinb.jfun.optics.PrismLaws;
+
+class BigDecimalOpticsTest {
+
+  @Test
+  void testPrismLawsBigDecimalToBigLong() {
+    final Prism<BigDecimal, Long> prism = BigDecimalOptics.bigDecimalToLong();
+
+    assertThat(PrismLaws.partialRoundTripOneWay(prism, BigDecimal.valueOf(Long.MAX_VALUE)))
+        .isTrue();
+    assertThat(PrismLaws.partialRoundTripOneWay(prism, BigDecimal.ZERO)).isTrue();
+    assertThat(PrismLaws.partialRoundTripOneWay(prism, BigDecimal.valueOf(Long.MIN_VALUE)))
+        .isTrue();
+
+    assertThat(PrismLaws.partialRoundTripOtherWay(prism, Long.MAX_VALUE)).isTrue();
+    assertThat(PrismLaws.partialRoundTripOtherWay(prism, 0L)).isTrue();
+    assertThat(PrismLaws.partialRoundTripOtherWay(prism, Long.MIN_VALUE)).isTrue();
+  }
+
+  @Test
+  void testPrismLawsBigDecimalToBigInteger() {
+    final Prism<BigDecimal, Integer> prism = BigDecimalOptics.bigDecimalToInteger();
+
+    assertThat(PrismLaws.partialRoundTripOneWay(prism, BigDecimal.valueOf(Integer.MAX_VALUE)))
+        .isTrue();
+    assertThat(PrismLaws.partialRoundTripOneWay(prism, BigDecimal.ZERO)).isTrue();
+    assertThat(PrismLaws.partialRoundTripOneWay(prism, BigDecimal.valueOf(Integer.MIN_VALUE)))
+        .isTrue();
+
+    assertThat(PrismLaws.partialRoundTripOtherWay(prism, Integer.MAX_VALUE)).isTrue();
+    assertThat(PrismLaws.partialRoundTripOtherWay(prism, 0)).isTrue();
+    assertThat(PrismLaws.partialRoundTripOtherWay(prism, Integer.MIN_VALUE)).isTrue();
+  }
+}
