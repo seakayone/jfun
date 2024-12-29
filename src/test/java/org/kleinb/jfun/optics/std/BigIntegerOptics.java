9b7823ad236f5a7cf832ce52670263cf4116438d
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-12-29 14:23:20 +0100

    add BigIntegerOptics

---
+++ src/test/java/org/kleinb/jfun/optics/std/BigIntegerOptics.java
@@ -0,0 +1,48 @@
+package org.kleinb.jfun.optics.std;
+
+import java.math.BigInteger;
+import org.kleinb.jfun.Function1;
+import org.kleinb.jfun.Option;
+import org.kleinb.jfun.Try;
+import org.kleinb.jfun.optics.Prism;
+
+public class BigIntegerOptics {
+  private BigIntegerOptics() {}
+
+  public static Prism<BigInteger, Long> bigIntegerToLong() {
+    return Prism.of(bi -> Try.of(bi::longValueExact).toOption(), BigInteger::valueOf);
+  }
+
+  public static Prism<BigInteger, Integer> bigIntegerToInteger() {
+    return Prism.of(
+        bi -> Try.of(bi::intValueExact).toOption(),
+        (Function1<Integer, BigInteger>) BigInteger::valueOf);
+  }
+
+  public static Prism<BigInteger, Character> bigIntegerToCharacter() {
+    final Function1<BigInteger, Option<Character>> getOption =
+        (BigInteger bi) -> Try.of(() -> ((char) bi.longValueExact())).toOption();
+    return Prism.of(getOption, (Function1<Character, BigInteger>) BigInteger::valueOf);
+  }
+
+  public static Prism<BigInteger, Byte> bigIntegerToByte() {
+    return Prism.of(
+        bi -> Try.of(bi::byteValueExact).toOption(),
+        (Function1<Byte, BigInteger>) BigInteger::valueOf);
+  }
+
+  public static Prism<BigInteger, Boolean> bigIntegerToBoolean() {
+    return Prism.of(
+        bi ->
+            Try.of(bi::intValueExact)
+                .toOption()
+                .flatMap(
+                    i ->
+                        switch (i) {
+                          case 0 -> Option.some(false);
+                          case 1 -> Option.some(true);
+                          default -> Option.none();
+                        }),
+        b -> b ? BigInteger.ONE : BigInteger.ZERO);
+  }
+}
---
+++ src/test/java/org/kleinb/jfun/optics/std/BigIntegerOpticsTest.java
@@ -0,0 +1,81 @@
+package org.kleinb.jfun.optics.std;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigInteger;
+import org.junit.jupiter.api.Test;
+import org.kleinb.jfun.optics.PrismLaws;
+
+class BigIntegerOpticsTest {
+
+  @Test
+  void testPrismLawsBigIntegerToLong() {
+    var prism = BigIntegerOptics.bigIntegerToLong();
+
+    assertThat(PrismLaws.partialRoundTripOneWay(prism, BigInteger.valueOf(Long.MAX_VALUE)))
+        .isTrue();
+    assertThat(PrismLaws.partialRoundTripOneWay(prism, BigInteger.ZERO)).isTrue();
+    assertThat(PrismLaws.partialRoundTripOneWay(prism, BigInteger.valueOf(Long.MIN_VALUE)))
+        .isTrue();
+
+    assertThat(PrismLaws.partialRoundTripOtherWay(prism, Long.MAX_VALUE)).isTrue();
+    assertThat(PrismLaws.partialRoundTripOtherWay(prism, 0L)).isTrue();
+    assertThat(PrismLaws.partialRoundTripOtherWay(prism, Long.MIN_VALUE)).isTrue();
+  }
+
+  @Test
+  void testPrismLawsBigIntegerToInteger() {
+    var prism = BigIntegerOptics.bigIntegerToInteger();
+
+    assertThat(PrismLaws.partialRoundTripOneWay(prism, BigInteger.valueOf(Integer.MAX_VALUE)))
+        .isTrue();
+    assertThat(PrismLaws.partialRoundTripOneWay(prism, BigInteger.ZERO)).isTrue();
+    assertThat(PrismLaws.partialRoundTripOneWay(prism, BigInteger.valueOf(Integer.MIN_VALUE)))
+        .isTrue();
+
+    assertThat(PrismLaws.partialRoundTripOtherWay(prism, Integer.MAX_VALUE)).isTrue();
+    assertThat(PrismLaws.partialRoundTripOtherWay(prism, 0)).isTrue();
+    assertThat(PrismLaws.partialRoundTripOtherWay(prism, Integer.MIN_VALUE)).isTrue();
+  }
+
+  @Test
+  void testPrismLawsBigIntegerToCharacter() {
+    var prism = BigIntegerOptics.bigIntegerToCharacter();
+
+    assertThat(PrismLaws.partialRoundTripOneWay(prism, BigInteger.valueOf(Character.MAX_VALUE)))
+        .isTrue();
+    assertThat(PrismLaws.partialRoundTripOneWay(prism, BigInteger.ZERO)).isTrue();
+    assertThat(PrismLaws.partialRoundTripOneWay(prism, BigInteger.valueOf(Character.MIN_VALUE)))
+        .isTrue();
+
+    assertThat(PrismLaws.partialRoundTripOtherWay(prism, Character.MAX_VALUE)).isTrue();
+    assertThat(PrismLaws.partialRoundTripOtherWay(prism, (char) 0)).isTrue();
+    assertThat(PrismLaws.partialRoundTripOtherWay(prism, Character.MIN_VALUE)).isTrue();
+  }
+
+  @Test
+  void testPrismLawsBigIntegerToByte() {
+    var prism = BigIntegerOptics.bigIntegerToByte();
+
+    assertThat(PrismLaws.partialRoundTripOneWay(prism, BigInteger.valueOf(Byte.MAX_VALUE)))
+        .isTrue();
+    assertThat(PrismLaws.partialRoundTripOneWay(prism, BigInteger.ZERO)).isTrue();
+    assertThat(PrismLaws.partialRoundTripOneWay(prism, BigInteger.valueOf(Byte.MIN_VALUE)))
+        .isTrue();
+
+    assertThat(PrismLaws.partialRoundTripOtherWay(prism, Byte.MAX_VALUE)).isTrue();
+    assertThat(PrismLaws.partialRoundTripOtherWay(prism, (byte) 0)).isTrue();
+    assertThat(PrismLaws.partialRoundTripOtherWay(prism, Byte.MIN_VALUE)).isTrue();
+  }
+
+  @Test
+  void testPrismLawsBigIntegerToBoolean() {
+    var prism = BigIntegerOptics.bigIntegerToBoolean();
+
+    assertThat(PrismLaws.partialRoundTripOneWay(prism, BigInteger.ONE)).isTrue();
+    assertThat(PrismLaws.partialRoundTripOneWay(prism, BigInteger.ZERO)).isTrue();
+
+    assertThat(PrismLaws.partialRoundTripOtherWay(prism, true)).isTrue();
+    assertThat(PrismLaws.partialRoundTripOtherWay(prism, false)).isTrue();
+  }
+}
