3b4093587279c71135b53e029e94598e26fbd06a
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-12-29 12:24:04 +0100

    add ValidationOptics

---
+++ src/main/java/org/kleinb/jfun/optics/std/ValidationOptics.java
@@ -0,0 +1,17 @@
+package org.kleinb.jfun.optics.std;
+
+import java.util.List;
+import org.kleinb.jfun.Validation;
+import org.kleinb.jfun.optics.PartialFunction;
+import org.kleinb.jfun.optics.Prism;
+
+public interface ValidationOptics {
+  static <E, A> Prism<Validation<E, A>, List<E>> invalid() {
+    return Prism.of(
+        PartialFunction.of(Validation::getError, Validation::isInvalid), Validation::invalid);
+  }
+
+  static <E, A> Prism<Validation<E, A>, A> valid() {
+    return Prism.of(PartialFunction.of(Validation::get, Validation::isValid), Validation::valid);
+  }
+}
---
+++ src/test/java/org/kleinb/jfun/optics/std/ValidationOpticsTest.java
@@ -0,0 +1,27 @@
+package org.kleinb.jfun.optics.std;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.kleinb.jfun.Validation;
+import org.kleinb.jfun.optics.Prism;
+import org.kleinb.jfun.optics.PrismLaws;
+
+class ValidationOpticsTest {
+  @Test
+  void testValidPrismLaws() {
+    final Prism<Validation<Throwable, Integer>, Integer> validPrism = ValidationOptics.valid();
+    assertThat(PrismLaws.partialRoundTripOneWay(validPrism, Validation.valid(42))).isTrue();
+    assertThat(PrismLaws.partialRoundTripOtherWay(validPrism, 42)).isTrue();
+  }
+
+  @Test
+  void testInvalidPrismLaws() {
+    final Prism<Validation<String, Integer>, List<String>> invalidPrism =
+        ValidationOptics.invalid();
+    assertThat(PrismLaws.partialRoundTripOneWay(invalidPrism, Validation.invalid("error")))
+        .isTrue();
+    assertThat(PrismLaws.partialRoundTripOtherWay(invalidPrism, List.of("error"))).isTrue();
+  }
+}
